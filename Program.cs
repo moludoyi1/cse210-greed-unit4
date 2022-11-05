1ce0025f36607d15fab64790d9eff325be3ed3b3
Michael <olu21003@byui.edu>
2022-11-05 11:01:55 -0600

    new projects

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace GreedGame
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            // var ScreenHeight = 480;
+            // var ScreenWidth = 800;
+            Raylib.InitWindow(800, 480, "Score: ");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.DrawText("Score: ", 12, 12, 20, Color.WHITE);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        public int Score()
+        {
+            
+        }
+    }
+}
