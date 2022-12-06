daa1fa3be02227568facfe8dea72a66fb67756df
Michael <olu21003@byui.edu>
2022-12-06 11:24:14 -0700

    working

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace GreedGame
             // var RandomColor = 
 
             Raylib.InitWindow(ScreenWidth, ScreenHeight, "GameObject");
-            Raylib.SetTargetFPS(80);
+            Raylib.SetTargetFPS(60);
 
             while (!Raylib.WindowShouldClose())
             {
@@ -24,24 +24,24 @@ namespace GreedGame
                 // add a new object to the screen every iteration of the game loop
                 var whichType =  Random.Next(3);
 
-                var randomY = Random.Next(-2, 2);
-                var randomX = Random.Next(-2, 2);
+                var randomY = Random.Next(1, 3);
+                var randomX = Random.Next(0, ScreenWidth);
 
-                var position = new Vector2(ScreenWidth/2, 0);
+                var position = new Vector2(randomX, 0);
 
                 switch (whichType) {
                     case 0:
                         Console.WriteLine("Creating a rock");
                         var rock = new GameRock(Color.GRAY, 8);
                         rock.Position = position;
-                        rock.Velocity = new Vector2(randomX, randomY);
+                        rock.Velocity = new Vector2(0, randomY);
                         Objects.Add(rock);
                         break;
                     case 1:
                         Console.WriteLine("Creating a gem");
                         var gem = new GameRock(Color.PURPLE, 5);
                         gem.Position = position;
-                        gem.Velocity = new Vector2(randomX, randomY);
+                        gem.Velocity = new Vector2(0, randomY);
                         Objects.Add(gem);
                         break;
                     default:
