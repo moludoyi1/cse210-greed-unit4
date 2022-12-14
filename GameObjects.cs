dce64ce59340908449af5e577c9eb4c7c86455ba
ballanm <burtonlog20@gmail.com>
2022-12-13 17:48:52 -0700

    Completion
    Worked and changed the objects

--- GameObjects.cs
+++ GameObjects.cs
@@ -1,50 +1,23 @@
 using Raylib_cs;
 using System.Numerics;
 
-class GameObject {
-    public Vector2 Position{get; set;} = new Vector2(0, 0);
-    public Vector2 Velocity{get; set;} = new Vector2(0, 1);
-
-    virtual public void Draw() {
-        // not available at the moment
-    }
-
-    public void Move() {
-        Vector2 NewPostion = this.Position;
-        NewPostion.X += Velocity.X;
-        NewPostion.Y += Velocity.Y;
-        this.Position = NewPostion;
-    }
-}
-
-
-class ColoredObject: GameObject {
-    public Color Color{get; set;}
-
-    public ColoredObject(Color color) {
-        Color = color;
-    }
-}
-
-class GameRock: ColoredObject {
-    public int Size {get; set;}
-    
-    public GameRock(Color color, int size): base(color) {
-        Size = size;
-    }
-
+class GameRock: MovementVertical{
+    // DRAW THE ROCKS
     override public void Draw() {
-        Raylib.DrawCircle((int)Position.X, (int)Position.Y, Size, Color);
+        Random rnd = new Random();
+        int RandInt = rnd.Next(20,780);
+
+        Raylib.DrawText("o", (int)Position.X, (int)Position.Y, 15, Color.BLUE);
     }
 }
 
-class GameGem: ColoredObject {
 
-    public GameGem(Color color, int size): base(color){    }
+class GameGem: MovementVertical{
+    // DRAW THE GEMS
+    override public void Draw() {
+        Random rnd = new Random();
+        int RandInt = rnd.Next(20,780);
 
-    public override void Draw()
-    {
-        Raylib.DrawRectangle((int)Position.X, (int)Position.Y, 20, 20, Color);
+        Raylib.DrawText("*", (int)Position.X , (int)Position.Y, 15, Color.RED);
     }
 }
-
--- Program.cs
+++ Program.cs
@@ -1,55 +1,109 @@
-﻿using Raylib_cs;
+using Raylib_cs;
 using System.Numerics;
 
-namespace GreedGame
-{
+class Main {
     static class Program
     {
         public static void Main()
         {
+            // Accessing variables through the constant class
             var ScreenHeight = 480;
             var ScreenWidth = 800;
-            var Objects = new List<GameObject>();
+            var Rocks = new List<GameRock>();
+            var Gems = new List<GameGem>();
             var Random = new Random();
-            // var RandomColor = 
 
-            Raylib.InitWindow(ScreenWidth, ScreenHeight, "GameObject");
-            Raylib.SetTargetFPS(60);
+            Raylib.InitWindow(ScreenWidth, ScreenHeight, "Greed");
+            Raylib.SetTargetFPS(20);
+            int score = 0;
+            var PlayerPosition = new Vector2 (400,450);
 
             while (!Raylib.WindowShouldClose())
             {
-                // set background color to balck
-                Raylib.ClearBackground(Color.BLACK);
-                
-                // add a new object to the screen every iteration of the game loop
-                var whichType =  Random.Next(300);
+                // INTRO TEXT 
+                // Raylib.DrawText("Move the red square to the blue square with the arrow keys!", 12, 12, 20, Color.BLACK);
+                Raylib.DrawText($"Score: {score}",12,12,15,Color.BLACK);
 
-                var randomY = Random.Next(1, 3);
-                var randomX = Random.Next(0, ScreenWidth);
+                var RockOrGem = Random.Next(2);
+                int PositionX = Random.Next(0, ScreenWidth);
+                var position = new Vector2 (PositionX , 0);
 
-                var position = new Vector2(randomX, 0);
-
-                switch (whichType) {
+                switch (RockOrGem){
+                    // If case is 0 it'll create a Rock
                     case 0:
-                        Console.WriteLine("Creating a rock");
-                        var rock = new GameRock(Color.GRAY, 8);
-                        rock.Position = position;
-                        rock.Velocity = new Vector2(0, randomY);
-                        Objects.Add(rock);
-                        break;
+                     var Rock = new GameRock();
+                     Rock.Position = position;
+                     Rock.Velocity = new Vector2(0,8);
+                     Rocks.Add(Rock);
+                     break;
+                    //  IF case 1 create a GEM
                     case 1:
-                        Console.WriteLine("Creating a gem");
-                        var gem = new GameRock(Color.PURPLE, 5);
-                        gem.Position = position;
-                        gem.Velocity = new Vector2(0, randomY);
-                        Objects.Add(gem);
+                        var Gem = new GameGem();
+                        Gem.Position = position;
+                        Gem.Velocity = new Vector2(0,8);
+                        Gems.Add(Gem);
                         break;
                     default:
                         break;
                 }
-                Raylib.BeginDrawing()
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+
+
+                // Draw all of the objects in their current location
+                foreach (var obj in Rocks) {
+                    obj.Draw();
+                }
+                foreach (var obj in Gems) {
+                    obj.Draw();
+                }
+                // Call the player class
+                var Player= new Player();
+                // Assign a position and a velocity to the player
+                Player.Position = PlayerPosition;
+                Player.Velocity = new Vector2(2, 0); 
+                Player playerObj = new Player();
+        // The player will be redrawn in a new position when the keyboard is pressed
+                Player.Draw();
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+                    PlayerPosition.X +=6;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+                    PlayerPosition.X -=6;
+                }
+                Raylib.EndDrawing();
+
+                // Move all of the objects to their next location
+                foreach (var obj in Rocks) {
+                    obj.MoveVertical();
+                }
+                foreach (var obj in Gems) {
+                    obj.MoveVertical();
+                }
+            // Define the hitbox for the player using a rectangle
+                var TheRectangle = new Rectangle(PlayerPosition.X,PlayerPosition.Y, 20, 20);
+            // Go through the Rocks and Gems list and check if they're inside the hitbox
+                foreach (var obj in Rocks.ToList()) {
+                    if (Raylib.CheckCollisionPointRec(obj.Position, TheRectangle)) {
+                        score -=1;
+                    //Remove object from list if collision occurs(Doesn't work when running through the foreach loop)
+                    Rocks.Remove(obj);
+                    }
+                }
+
+                foreach (var obj in Gems.ToList()) {
+                    if (Raylib.CheckCollisionPointRec(obj.Position, TheRectangle)) {
+                        score +=1;
+                    //Remove object from list if collision occurs(Doesn't work when running through the foreach loop)
+                    Gems.Remove(obj);
+                    }
+                }
 
             }
+
+            Raylib.CloseWindow();
         }
     }
 }
